3590323c2bb8c82be23f3c2cb6826e8e324ec7e1
Airat Khisamiev <aira3t@gmail.com>
2023-08-17 22:43:03 +0200

    Update dockerfile

--- packages/frontend/src/constants/Config.ts
+++ packages/frontend/src/constants/Config.ts
@@ -1,3 +1,3 @@
-export const APP_URL = process.env.VITE_APP_URL; // will be replaces to env later
+export const APP_URL = import.meta.env.VITE_APP_URL; // will be replaces to env later
 
 export const SERVER_UP_WAIT_TIME_MSEC = 2000;
--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,14 +1,18 @@
-import { defineConfig } from "vite";
-import { resolve } from "node:path";
-import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from 'vite';
+import { resolve } from 'node:path';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      src: resolve(__dirname, "./src"),
-      "src/*": resolve(__dirname, "./src/*"),
+export default defineConfig(({ mode }) => {
+  process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        src: resolve(__dirname, './src'),
+        'src/*': resolve(__dirname, './src/*'),
+      },
     },
-  },
+  };
 });
